ef8b55691d34d143b879f92447e878502f760162
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-28 00:12:41 -0500

    Add basic infinite tree trunk and object pooling

---
+++ Assets/Scripts/GameObjectPool.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool : MonoBehaviour
+{
+    public int startingPoolSize;
+    public GameObject[] prefabs;
+
+    private List<GameObject> pool;
+    private bool notInitialized = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (notInitialized)
+            Init();
+    }
+
+    private void Init()
+    {
+        pool = new List<GameObject>();
+        for (int i = 0; i < startingPoolSize; i++)
+        {
+            pool.Add(Instantiate(prefabs[Random.Range(0, prefabs.Length)]));
+        }
+
+        foreach (GameObject go in pool)
+        {
+            go.SetActive(false);
+        }
+
+        notInitialized = false;
+    }
+
+    public Transform GetNext()
+    {
+        if (notInitialized)
+            Init();
+
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (!pool[i].activeInHierarchy)
+            {
+                pool[i].SetActive(true);
+                return pool[i].transform;
+            }
+        }
+
+        pool.Add(Instantiate(prefabs[Random.Range(0, prefabs.Length)]));
+        pool[pool.Count - 1].SetActive(true);
+        return pool[pool.Count - 1].transform;
+    }
+
+    public void TrimToSize(int newSize)
+    {
+        foreach (GameObject go in pool)
+        {
+            if (pool.Count <= newSize)
+            {
+                break;
+            }
+
+            if (!go.activeInHierarchy)
+            {
+                pool.Remove(go);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TreeTrunk.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTrunk : MonoBehaviour
+{
+    public GameObject treeTrunkPartPrefab;
+    public GameObjectPool pool;
+
+    public float prefabHeight;
+
+    public float distanceBeforeLoadingNextPart = 50f;
+
+    public int initialPartsToSpawn = 3;
+    public int maxPartsToSpawn = 10;
+    private List<Transform> spawnedParts;
+
+    private Vector2 nextOffset = Vector2.zero;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        spawnedParts = new List<Transform>();
+        pool = GetComponent<GameObjectPool>();
+        for (int i = 0; i < initialPartsToSpawn; i++)
+        {
+            SpawnPart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextOffset.y - cam.transform.position.y < distanceBeforeLoadingNextPart)
+        {
+            SpawnPart();
+        }
+    }
+
+    private void SpawnPart()
+    {
+        Transform part = pool.GetNext();
+
+        part.position = nextOffset;
+
+        nextOffset = new Vector2(part.position.x, part.position.y + prefabHeight);
+
+        spawnedParts.Add(part);
+
+        if (spawnedParts.Count > maxPartsToSpawn)
+        {
+            spawnedParts[0].gameObject.SetActive(false);
+            spawnedParts.RemoveAt(0);
+        }
+    }
+}
