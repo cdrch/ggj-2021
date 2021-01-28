f24646905d4c1cfdc25e682d2f78c1717b775d4c
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-27 23:31:54 -0500

    Add prototyping art and basic player controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Vector2 movementInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        // Get movement input from axes
+        movementInput.x = Input.GetAxis("Horizontal");
+        movementInput.y = Input.GetAxis("Vertical");
+
+        // Add normalized movement to position
+        transform.position += (Vector3)movementInput.normalized * speed * Time.deltaTime;
+    }
+}
