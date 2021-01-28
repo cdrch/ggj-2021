fb31438034d92952b823f6e0d18c0f64226b2f73
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-28 00:36:56 -0500

    Add basic traps and health system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 1;
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    private void CheckForDeath()
+    {
+        if (health > 0)
+            return;
+
+        // handle death
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        CheckForDeath();
+    }
+}
