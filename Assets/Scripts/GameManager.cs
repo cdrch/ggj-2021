a5a356a95a85e7ec94585cbf8a7a712480ee97e9
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-28 19:58:20 -0500

    Add GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance;
+
+    public int difficultyLevel = 1;
+
+    public float elevatorSpeed = 1f;
+    public float elevatorSpeedChangePerDifficultyLevel = 0.25f; 
+    // each level, elevator speed increases by elevatorSpeed * elevatorSpeedChangePerDifficultyLevel
+
+    private Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
