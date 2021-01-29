974bed8d6798d71046b95556c27faf94078f6d23
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-29 15:21:08 -0500

    Update tree spawning to work in both directions

--- Assets/Scripts/TreeTrunk.cs
+++ Assets/Scripts/TreeTrunk.cs
@@ -9,18 +9,19 @@ public class TreeTrunk : MonoBehaviour
     public GameObjectPool rightTrunkPool;
 
     public float prefabHeight = 4.9375f;
+    public float prefabWidth = 4.5859375f;
     public float prefabHalfWidth = 4.5859375f / 2f;
 
     public float distanceBeforeLoadingNextPart = 50f;
 
     public int initialPartsToSpawn = 6;
-    public int maxPartsToSpawn = 10;
+    public int maxPartsToSpawn;
     public int spawnCount = 0;
 
-    private List<Transform> spawnedParts;
+    public List<Transform> spawnedParts; // TODO: make this private again after debugging
 
     private float nextOffsetYUp = 0f;
-    private float nextOffsetYDown = 0f; // TODO: use this so parts spawn and unspawn going back down as well
+    private float nextOffsetYDown; // TODO: use this so parts spawn and unspawn going back down as well
     // Alternative: just leave a row of deadliness at the lowest current point if you try to go back?
 
     private Camera cam;
@@ -28,6 +29,8 @@ public class TreeTrunk : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        nextOffsetYDown = 0f;
+
         cam = Camera.main;
         spawnedParts = new List<Transform>();
         leftTrunkPool = transform.Find("Left Trunk Parts").GetComponent<GameObjectPool>();
@@ -37,45 +40,144 @@ public class TreeTrunk : MonoBehaviour
         Instantiate(treeTrunkBase, Vector3.zero, Quaternion.identity);
 
         // Place the initial trunk pieces
+        DecreaseOffsetDownByOneLayer();
         for (int i = 0; i < initialPartsToSpawn; i++)
         {
-            SpawnNextTrunkLayer();
+            AddNewLayerGoingUp();
+            AddNewLayerGoingDown();
         }
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (nextOffsetYUp - cam.transform.position.y < distanceBeforeLoadingNextPart)
+        CheckIfOffsetUpIsCloseEnoughToNeedANewLayer();
+        CheckIfOffsetDownIsCloseEnoughToNeedANewLayer();
+
+        CheckIfOffsetUpIsFarEnoughToDespawn();
+        CheckIfOffsetDownIsFarEnoughToDespawn();
+    }
+
+    private void RemoveTopLayer()
+    {
+        spawnedParts[spawnedParts.Count - 1].gameObject.SetActive(false);
+        spawnedParts.RemoveAt(spawnedParts.Count - 1);
+
+        spawnedParts[spawnedParts.Count - 1].gameObject.SetActive(false);
+        spawnedParts.RemoveAt(spawnedParts.Count - 1);
+
+        DecreaseOffsetUpByOneLayer();
+    }
+
+    private void RemoveBottomLayer()
+    {
+        spawnedParts[0].gameObject.SetActive(false);
+        spawnedParts.RemoveAt(0);
+
+        spawnedParts[0].gameObject.SetActive(false);
+        spawnedParts.RemoveAt(0);
+
+        IncreaseOffsetDownByOneLayer();
+    }
+
+    private void CheckIfOffsetUpIsCloseEnoughToNeedANewLayer()
+    {
+        if (cam.transform.position.y + distanceBeforeLoadingNextPart >= nextOffsetYUp)
         {
-            SpawnNextTrunkLayer();
+            AddNewLayerGoingUp();
         }
     }
 
-    private void SpawnNextTrunkLayer()
+    private void CheckIfOffsetDownIsCloseEnoughToNeedANewLayer()
+    {
+        if (cam.transform.position.y - distanceBeforeLoadingNextPart <= nextOffsetYDown + prefabHeight)
+        {
+            //Debug.Log("no " + cam.transform.position.y + " - " + distanceBeforeLoadingNextPart + " = " + (cam.transform.position.y - distanceBeforeLoadingNextPart) + " | " + nextOffsetYDown + " + " + prefabHeight + " = " + (nextOffsetYDown + prefabHeight));
+            AddNewLayerGoingDown();
+        }
+    }
+
+    private void CheckIfOffsetUpIsFarEnoughToDespawn()
+    {
+        if (cam.transform.position.y + distanceBeforeLoadingNextPart < nextOffsetYUp)
+        {
+            RemoveTopLayer();
+        }
+    }
+
+    private void CheckIfOffsetDownIsFarEnoughToDespawn()
+    {
+        if (cam.transform.position.y - distanceBeforeLoadingNextPart > nextOffsetYDown + prefabHeight)
+        {
+            RemoveBottomLayer();
+        }
+    }
+
+    private void AddNewLayerGoingUp()
     {
         Transform leftPart = leftTrunkPool.GetNext();
         Transform rightPart = rightTrunkPool.GetNext();
 
         leftPart.position = new Vector2(-prefabHalfWidth, nextOffsetYUp);
-        rightPart.position = new Vector2(prefabHalfWidth, nextOffsetYUp);        
+        rightPart.position = new Vector2(prefabHalfWidth, nextOffsetYUp);
 
-        nextOffsetYUp = nextOffsetYUp + prefabHeight;
+        IncreaseOffsetUpByOneLayer();
 
         spawnedParts.Add(leftPart);
         spawnedParts.Add(rightPart);
 
         spawnCount += 1;
-
-        if (spawnedParts.Count > maxPartsToSpawn)
-        {
-            spawnedParts[0].gameObject.SetActive(false);
-            spawnedParts.RemoveAt(0);
-
-            spawnedParts[0].gameObject.SetActive(false);
-            spawnedParts.RemoveAt(0);
-        }
+        //Debug.Log("Up: " + nextOffsetYUp);
     }
+
+    private void AddNewLayerGoingDown()
+    {
+        Transform leftPart = leftTrunkPool.GetNext();
+        Transform rightPart = rightTrunkPool.GetNext();
+
+        leftPart.position = new Vector2(-prefabHalfWidth, nextOffsetYDown);
+        rightPart.position = new Vector2(prefabHalfWidth, nextOffsetYDown);
+
+        DecreaseOffsetDownByOneLayer();
+
+        spawnedParts.Insert(0, leftPart);
+        spawnedParts.Insert(0, rightPart);
+
+        spawnCount += 1;
+        //Debug.Log("Down: " + nextOffsetYDown);
+    }
+
+    private void IncreaseOffsetDownByOneLayer()
+    {
+        nextOffsetYDown += prefabHeight;
+    }
+
+    private void DecreaseOffsetDownByOneLayer()
+    {
+        nextOffsetYDown -= prefabHeight;
+    }
+
+    private void IncreaseOffsetUpByOneLayer()
+    {
+        nextOffsetYUp += prefabHeight;
+    }
+
+    private void DecreaseOffsetUpByOneLayer()
+    {
+        nextOffsetYUp -= prefabHeight;
+    }
+
+    /*private void SpawnNextTrunkLayer(bool goingUp)
+    {
+        // iterate over imaginary grid of trunk parts
+        for (float x = -cam.orthographicSize * 2 * prefabWidth; x <= cam.orthographicSize * 2 * prefabWidth; x += prefabWidth) // consider Floor/Ceil in the x <= check?
+        {
+            for (float y = -cam.orthographicSize * 2 * prefabHeight; y <= cam.orthographicSize * 2 * prefabHeight; y += prefabHeight)
+            {
+                if (
+            }
+        }
+    }*/
 }
 
 public enum TreePart
