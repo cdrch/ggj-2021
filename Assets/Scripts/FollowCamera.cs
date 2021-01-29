1fa92ab7e5dbfdd5793288cf4d5e25736575b80a
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-28 20:14:33 -0500

    Update camera to no longer be a child of player

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    // TODO: consider making this a multi-target camera
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
