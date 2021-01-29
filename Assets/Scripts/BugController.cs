62961ae891de405e0b74c3a1f9888f76aff25cd7
Nikhil Rao <sourcenrao@gmail.com>
2021-01-29 01:45:50 -0500

    Add directional modifiers and wall collision.

--- Assets/Scripts/BugController.cs
+++ Assets/Scripts/BugController.cs
@@ -9,9 +9,14 @@ public class BugController : MonoBehaviour
 
     public float tiles = 0f;
 
-    public float difficultyfactor = 10f; // Base speed divisor (can be modified)
+    public float difficultyFactor = 10f; // Base speed divisor (can be modified)
 
-    public float pathdistance = 0f; // Limit distance from spawn point
+    public int[] direction = new int[] { -1, 1 };
+
+    public int directionMultiplierx = 0;
+    public int directionMultipliery = 0;
+
+    public float angleRandomizer = 0f;
 
     private Vector2 movement;
 
@@ -29,22 +34,49 @@ public class BugController : MonoBehaviour
 
             if (tiles < 50)
             {
-                speed *= tiles / difficultyfactor;
-                pathdistance = Random.Range(tiles/5, tiles/5 + 3);
+                speed *= tiles / difficultyFactor;
             }
             else
             {
-                speed *= 50 / difficultyfactor;
-                pathdistance = Random.Range(3, 13);
+                speed *= 50 / difficultyFactor;
             }
-        movement.x = speed;
-        movement.y = speed;
 
+        directionMultiplierx = direction[Random.Range(0, 2)];
+        directionMultipliery = direction[Random.Range(0, 2)];
+        angleRandomizer = Random.Range((float)0.7, (float)1.0);
+
+        movement.x = speed * directionMultiplierx * angleRandomizer;
+        movement.y = speed * directionMultipliery * (1.7f - angleRandomizer);
+        movement.Normalize();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += (Vector3)movement.normalized;
+        transform.position += (Vector3)movement * Time.deltaTime;
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+        {
+            movement *= -1;
+            return;
+        }
+
+        Health h = collision.GetComponent<Health>();
+
+        if (h == null)
+        {
+            Debug.Log("There's a missing health component on " + collision.name + "!");
+            return;
+        }
+
+        h.TakeDamage(1);
+
+        Destroy(gameObject); // TODO: replace with pooling later
+
+    }
+
 }
