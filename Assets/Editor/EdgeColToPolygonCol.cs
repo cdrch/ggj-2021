1e891b4443bb3f572ef69c6e73fd4c7e9d836f7c
Carter David Henning <carterdavidhenning@gmail.com>
2021-01-30 14:30:13 -0500

    Add custom inspector buttons for colliders
    These buttons let you switch between edge and polygon colliders (2D) while keeping the points. This lets you use the autogenerated collider from polygon colliders in an edge collider.

---
+++ Assets/Editor/EdgeColToPolygonCol.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(EdgeCollider2D))]
+public class EdgeColToPolygonCol : Editor
+{
+    public bool convertToTriggers;
+
+    private void ConvertEdgeToPoly(EdgeCollider2D col)
+    {
+        List<Vector2> pointsList = new List<Vector2>();
+        pointsList.AddRange(col.points);
+
+        PolygonCollider2D poly = col.gameObject.AddComponent<PolygonCollider2D>();
+
+        poly.pathCount = 1;
+        poly.SetPath(0, pointsList.ToArray());
+
+        if (convertToTriggers)
+            poly.isTrigger = true;
+
+        DestroyImmediate(col);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EdgeCollider2D col = (EdgeCollider2D)target;
+
+        DrawDefaultInspector();
+
+        //myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+        //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+
+        if (GUILayout.Button("Convert To Poly Collider (Destructive)"))
+        {
+            ConvertEdgeToPoly(col);
+        }
+    }
+}
---
+++ Assets/Editor/PolygonColToEdgeCol.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(PolygonCollider2D))]
+public class PolygonColToEdgeCol : Editor
+{
+    public bool convertToTriggers;
+    
+    private void ConvertPolyToEdge(PolygonCollider2D col)
+    {
+        List<Vector2> pointsList = new List<Vector2>();
+        pointsList.AddRange(col.points);
+        pointsList.Add(col.points[0]); // connect the start and end
+        
+        EdgeCollider2D edge = col.gameObject.AddComponent<EdgeCollider2D>();       
+
+        edge.points = pointsList.ToArray();
+
+        if (convertToTriggers)
+            edge.isTrigger = true;
+
+        DestroyImmediate(col);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        PolygonCollider2D col = (PolygonCollider2D)target;
+
+        DrawDefaultInspector();
+
+        //myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+        //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+
+        if (GUILayout.Button("Convert To Edge Collider (Destructive)"))
+        {
+            ConvertPolyToEdge(col);
+        }
+    }
+}
